b04d1c59d886411d1f96b8f06f0e175c84f2a4ae
heloisabarbosanascimento-cloud <heloisa.barbosa.nascimento@escola.pr.gov.br>
2026-05-08 11:02:11 -0300

    versão 1,0

---
+++ app.js
@@ -0,0 +1,4 @@
+  let mensagem = "Bom dia! Que seu dia seja leve, produtivo e cheio de sorrisos. 😊🌻";
+  function mostrarMensagem() {
+            document.querySelector("#msg").textContent = mensagem;
+        }
